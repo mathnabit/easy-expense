cee79cfc7884dd7f1f0bd4a93bc52afad48c5d08
Omar AIT ABDERRAHMAN <omar.atbd@gmail.com>
2023-06-13 19:13:14 +0100

    feat: scan files and get transactions

--- app/App.php
+++ app/App.php
@@ -2,4 +2,40 @@
 
 declare(strict_types=1);
 
-// Logic Code
+/* Logic Code */
+
+/* scan and return transaction files from directory */
+function getTransactionFiles(string $dirPath):array 
+{
+    $files = [];
+
+    foreach (scandir($dirPath) as $file) {
+        if (is_dir($file)) {
+            continue;
+        }
+        $files[] = $dirPath.$file;
+    }
+
+    return $files;
+}
+
+/* read each file and extract transactions from it */
+function getTransaction(string $fileName):array
+{
+    // verify existence of file
+    if (! file_exists($fileName)) {
+        trigger_error("File $fileName doesn't exist", E_USER_ERROR);
+    }
+
+    $file = fopen($fileName, 'r');
+    // read the first line (to discard it) that it's a header, not a transaction
+    fgetcsv($file);
+
+    $transactions = [];
+    // read line by line from the file with fgetcsv function
+    while (($transaction = fgetcsv($file)) !== false) {
+        $transactions[] = $transaction;
+    }
+
+    return $transactions;
+}
